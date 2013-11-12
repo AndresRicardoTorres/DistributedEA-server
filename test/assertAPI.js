6a42b73410d9c449e2e72cef011649e0e00a029a
andresricardotorres <torresandresricardo@gmail.com>
2013-11-12 11:53:21 -0500

    Agrego prueba con conexion a base de datos

--- test/assertAPI.js
+++ test/assertAPI.js
@@ -1,4 +1,8 @@
 var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+
+var uri_mongodb_connection = 'mongodb://177.71.196.106:27017/agmp';
+var mongodb_collection_config = 'configuracion';
 
 exports.assertStatus = function (code) {
     return function (error, response, body) {
@@ -9,11 +13,45 @@ exports.assertStatus = function (code) {
 
 exports.assertJSONResponse = function(obj_test){
     return function(error, response, body){
-        var obj_repsonse = JSON.parse(body);
-        assert.isObject(obj_repsonse);
-        for(var i in obj_test){
-            assert.include (obj_repsonse, i);
-            assert.equal(obj_repsonse[i],obj_test[i]);
+        var obj_repsonse = null;
+        try{
+            obj_repsonse = JSON.parse(body);
+        }catch(e){}
+        assert.isNotNull(obj_repsonse,"La respuesta debe ser un JSON valido");
+        if(obj_repsonse){
+            assert.isObject(obj_repsonse);
+            for(var i in obj_test){
+                assert.include (obj_repsonse, i);
+                assert.equal(obj_repsonse[i],obj_test[i]);
+            }
         }
+    }
+}
+
+exports.assertMongodbFindOne = function(query){
+    return function(error, response, body){
+        if(error) {
+            console.log(error);
+            return;
+        }
+        
+        MongoClient.connect(uri_mongodb_connection, function(err, db) {
+            assert.isNotNull(err,"Hubo error en la conexion de MongoDB");
+            if(err) {
+                console.log(err);
+                return;
+            }
+            //console.log("44");
+            db.collection(mongodb_collection_config).findOne(query,function(err,doc){
+                //console.log("46");
+                assert.isNotNull(err,"Hubo error en la consulta a MongoDB");
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(doc,"51");
+                //assert.isNotNull(doc,"El documento no debe estar vacio");
+            });
+        });
     }
 }
--- test/proyect_crud.js
+++ test/proyect_crud.js
@@ -16,17 +16,43 @@ function make_random_string(length)
 var nombre_proyecto_prueba = make_random_string(10);
 var ruta_nuevo_proyecto = "nuevo_proyecto";
 
+///Variables tmp
+var MongoClient = require('mongodb').MongoClient;
+var uri_mongodb_connection = 'mongodb://177.71.196.106:27017/agmp';
+var mongodb_collection_config = 'configuracion';
+var assert = require('assert');
+var DB =null;
+///END variables tmp
+
 vows.describe('creacion_proyecto').addBatch({
     "Creacion correcta con solo opciones obligatorias":{
         topic: topicAPI.post(ruta_nuevo_proyecto,{"proyecto":nombre_proyecto_prueba}),
         "Deberia enviar codigo OK": assertAPI.assertStatus(200),
         "Deberia responder OK en JSON" : assertAPI.assertJSONResponse({"ok":true}),
-        "Deberia estar en la BD" : function(){
-            
-            
+        "Deberia estar en la BD" :  {
+            topic: function(error, response, body){
+                var topicTHIS = this;
+                var query = {"nombre":nombre_proyecto_prueba};
+                MongoClient.connect(uri_mongodb_connection, function(err, db) {
+                    DB = db;
+                    //assert.isNotNull(err,"Hubo error en la conexion de MongoDB");
+                    if(err) {
+                        console.log(err);
+                        return;
+                    }
+                    //console.log("44");
+                    db.collection(mongodb_collection_config).findOne(query,topicTHIS.callback);
+                });
+                
+                
+            },
+            "El documento no debe ser null": function(err,doc){
+                DB.close();
+                assert.isNotNull(doc,"El documento no debe estar vacio");
+            }
         }
     },
-    "EL nombre del proyecto es obligatorio":{
+    "El nombre del proyecto es obligatorio":{
         topic: topicAPI.post(ruta_nuevo_proyecto,{}),
         "Deberia enviar codigo de error": assertAPI.assertStatus(400)
     },
