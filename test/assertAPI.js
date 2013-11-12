5f44a13fef55cf74a723a69bbc946e304d0b85ed
andresricardotorres <torresandresricardo@gmail.com>
2013-11-11 20:29:10 -0500

    inicio segunda prueba

--- test/assertAPI.js
+++ test/assertAPI.js
@@ -1,9 +1,19 @@
 var assert = require('assert');
 
 exports.assertStatus = function (code) {
-    return function (e, res) {
-        assert.isObject(res);
-       // console.log(res);
-        assert.equal (res.statusCode, code);
+    return function (error, response, body) {
+        assert.isObject(response);
+        assert.equal (response.statusCode, code);
     }    
+}
+
+exports.assertJSONResponse = function(obj_test){
+    return function(error, response, body){
+        var obj_repsonse = JSON.parse(body);
+        assert.isObject(obj_repsonse);
+        for(var i in obj_test){
+            assert.include (obj_repsonse, i);
+            assert.equal(obj_repsonse[i],obj_test[i]);
+        }
+    }
 }
--- test/proyect_crud.js
+++ test/proyect_crud.js
@@ -3,11 +3,33 @@ var vows = require('vows');
 var assertAPI = require("./assertAPI");
 var topicAPI = require("./topicAPI");
 
+function make_random_string(length)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+var nombre_proyecto_prueba = make_random_string(10);
+var ruta_nuevo_proyecto = "nuevo_proyecto";
+
 vows.describe('creacion_proyecto').addBatch({
+    "Creacion correcta con solo opciones obligatorias":{
+        topic: topicAPI.post(ruta_nuevo_proyecto,{"proyecto":nombre_proyecto_prueba}),
+        "Deberia enviar codigo OK": assertAPI.assertStatus(200),
+        "Deberia responder OK en JSON" : assertAPI.assertJSONResponse({"ok":true}),
+        "Deberia estar en la BD" : function(){
+            
+        }
+    },
     "EL nombre del proyecto es obligatorio":{
-        topic: topicAPI.post("nuevo_proyecto",{}),
+        topic: topicAPI.post(ruta_nuevo_proyecto,{}),
         "Deberia enviar codigo de error": assertAPI.assertStatus(400)
-    }
+    },
+    "Crear un proyecto con el nombre de uno ya creado":{}
   //  }).export(module); // Export the Suite;
 }).run();
 
