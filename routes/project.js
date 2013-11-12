c8f294dab72edcfcc3097bb14037f03c5d4a99b2
andresricardotorres <torresandresricardo@gmail.com>
2013-11-12 16:14:34 -0500

    Se convierte el parametro tamano_poblacion en un entero

--- routes/project.js
+++ routes/project.js
@@ -5,20 +5,27 @@ function ProjectHandler (db) {
     var objProjectsDAO = new ProjectsDAO(db);
     
     this.handleNewProject= function(req, res, next) {
-        console.log("handleNewProject");
-        console.log(typeof req.body.proyecto,"typeof req.body.proyecto");
+        
         if(typeof req.body.proyecto === "undefined"){
             console.log(400);
             res.status(400);
-            return res.send("{Valor de proyecto obligatorio}");
+            var respuesta = {};
+            respuesta.error = "Valor de proyecto obligatorio";
+            return res.send(JSON.stringify(respuesta));
         }
         
         var nombre = req.body.proyecto;
         var funcion_crear_cromosoma = req.body.crear_cromosoma;
+        var tamano_poblacion = parseInt(req.body.tamano_poblacion);
         
-        console.log(req.body,'req.body');
+        ///TODO: validar los tipos de datos
         
-        var nuevo_proyecto = {"proyecto":nombre,"funcion_crear_cromosoma":funcion_crear_cromosoma};
+        
+        var nuevo_proyecto = {
+                                "proyecto":nombre,
+                                "funcion_crear_cromosoma":funcion_crear_cromosoma,
+                                "tamano_poblacion" : tamano_poblacion
+                                };
         
         objProjectsDAO.newProject(nuevo_proyecto,function(error,doc){
             console.log(error,"error");
