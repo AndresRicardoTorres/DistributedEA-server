ad28e4c16a16d0d176d6d1dbc7175b43ca05a7e8
Your Name <you@example.com>
2013-12-16 12:04:28 -0500

    quitanto express

--- app.js
+++ app.js
@@ -1,59 +1,74 @@
-var express = require('express');
-var app = express(); // Web framework to handle routing requests
+var http = require('http');
+var qs = require('querystring');
 var MongoClient = require('mongodb').MongoClient // Driver for connecting to MongoDB
 var Server = require("./Server.js");
-
 
 MongoClient.connect('mongodb://eva05.local:37017,eva04.local:37017,eva03.local:37017/agmp', function(err, db) {
     if(err) {console.log(err);}
     
     var aServer = Server(db,function(isReady){
       
-//       console.log(isReady,'isReady');
+
       if(isReady){
-	// Express middleware to populate 'req.body' so we can access POST variables
-	app.use(express.json({limit: '50mb'}));
-	app.use(express.urlencoded({limit: '50mb'}));
-      
-	app.post("/",function(req, res, next){
-// 	  console.log(req.body ,'req.body');
-	  if(typeof req.body.action != 'undefined'){
-	    switch(req.body.action){
+	
+	function process(req,res){
+	  console.log("\n---ACTION "+req.action+'---');
+	  console.log(req,"REQ");
+	  if(typeof req.action != 'undefined'){
+	    switch(req.action){
 	      case 'request':
-		  this.handleRequest(req.body,function(subPopulation,project,actualGeneration,estimatedTime){
+		  this.handleRequest(req,function(subPopulation,project,actualGeneration,estimatedTime){
 		    if(project == null)
 		    {
 		      var respuesta = {finalized:true};		      
 		    }else{
-// 		      console.log(project,'project');
-		      console.log(subPopulation.length,"CANTIDAD A PROCESAR");
+
+		      console.log("Envio "+subPopulation.length+" individuos");
 		      var respuesta = {
 			generation:actualGeneration,
 			subPopulation:subPopulation,
 			estimatedTime:estimatedTime
 		      };
-		      if(req.body.assignedProject == 'false'){
+		      if(req.assignedProject == 'false'){
 			respuesta.assignedProject=project;
 		      }
 		    }
-		    console.log("handleRequest"+JSON.stringify(respuesta ));
-		    res.send(JSON.stringify(respuesta ));
+		    
+		    res.end(JSON.stringify(respuesta ));
 		  });
 		break;
-	      case 'deliver' :
-		  console.log("deliver");
-		  this.handleDeliver(req.body,function(){
-		     res.send(JSON.stringify({ok:true}));
+	      case 'deliver' :		  
+		  console.log("Recibo "+req.newChromosomes.length+" individuos");
+		  if(!req.newChromosomes instanceof Array){
+		    console.log(req);
+		  }
+		  if(typeof req.newChromosomes.length)
+		  this.handleDeliver(req,function(){
+		     res.end(JSON.stringify({ok:true}));
 		  });
 		break;
 	    }
 	  }
-	});
+	}
 	
 	///process.env.PORT variable for use in cloud9
-	var port = process.env.PORT || 8000;
-	app.listen(port);
+	var port = 8000;
+	
+	http.createServer(function (request, response) {
+	  if (request.method == 'POST') {
+	    var requestBody = '';
+	    request.on('data', function(data) {
+	      requestBody += data;	      
+	    });
+	    request.on('end', function() {
+	      console.log(requestBody,'requestBody');
+	      var formData = qs.parse(requestBody);
+	      process(formData,response);	      
+	    });
+	  }
+	}).listen(port);	
 	console.log('Server listening on port '+port);
+	
       }else{
 	console.log("Projects not found");
 	db.close();
