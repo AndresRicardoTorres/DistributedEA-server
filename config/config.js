684bb1a4304cdf07909d9ec888564cf0fd4f8400
Andres Ricardo Torres Martinez <torresandresricardo@gmail.com>
2013-12-17 16:35:06 -0500

    Queue :)

---
+++ config/config.js
@@ -0,0 +1,10 @@
+var config1 = {
+  urlMongo : 'mongodb://eva05.local:37017,eva04.local:37017,eva03.local:37017/agmp',
+  httpPort : 8000
+};
+
+var config2 = {
+  urlMongo : 'mongodb://localhost:27017/agmp',httpPort : 8000
+};
+
+module.exports = config2;
