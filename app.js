fdcc5d22d1e934c1c2226e977e12a8cac6b84254
Erika Suárez Valencia <marvinesv@gmail.com>
2014-01-22 17:30:08 -0500

    Pruebas rancias. Comparar con ejecuciones en casa

--- app.js
+++ app.js
@@ -20,16 +20,24 @@ MongoClient.connect(configuration.urlMongo, function(err, db) {
         initialTime = new Date();
       if (request.method == 'POST') {
 	      var requestBody = '';
-        request.on('data', function(data) {requestBody +=data;});
+        request.on('data', function(data) {requestBody +=data;
+	  console.log("DATA"+new Date().getTime())	  
+	});
         request.on('end', function() {	      
       	  var formData = qs.parse(requestBody);
+ 	  formData.dateTest = parseInt(formData.dateTest);
+	  //console.log("formData.dateTest"+formData.dateTest);
+		   console.log("formData.dateTest"+formData.dateTest);
+	  console.log("new test => "+(new Date().getTime()-formData.dateTest));
+		   if(formData.action == 'deliver'){
+		   timeNewTest+=(new Date().getTime()-formData.dateTest);}
       	  var aTask = {
       	    server:THIS,
       	    data:formData,
       	    response:response
       	  }
           ///Write in database asyncronius off the tasks
-        	THIS.writeInDB();
+        	//THIS.writeInDB();
       	  aQueue.push(aTask,function(err){            
       	    if(err)console.error("ERROR en push "+err);
             queueCount++;            
@@ -45,19 +53,33 @@ MongoClient.connect(configuration.urlMongo, function(err, db) {
     console.log('Start time :'+new Date());
 
   });
-  
+  var timeRequest=0;
+  var timeDeliver=0;
   var queueCount = 0;
+  var timeNewTest = 0;
   var aQueue = async.queue(function(task,callback){
     
     lapTime = new Date();
     task.server.processCommunication(task.data,function(error,answer){
+      if (answer== JSON.stringify({ok:true})){
+	timeDeliver+=new Date()-lapTime;	
+      }
+      else{
+	timeRequest+=new Date()-lapTime;
+	console.log("lap => "+(new Date()-lapTime)/1000);
+      }
       task.response.end(answer);
       
       if(answer == JSON.stringify({finalized:true})){          
         	var endTime = new Date();
+		console.info("Initial Time => "+ initialTime);
         	var time = (endTime - initialTime)/1000;
           lapTotalTime=lapTotalTime/1000;
-
+	  console.info("timeNewTest: "+timeNewTest/1000);
+	  console.info("request time: "+task.server.totalTimeRequest/1000)
+	  console.info("request time (round): "+timeRequest/1000)
+	  console.info("deliver time: "+task.server.totalTimeDeliver/1000)
+	  console.info("deliver time (round): "+timeDeliver/1000)
         	console.info("End Time => "+ endTime);
           console.info("Total Duration "+time+" in seconds");
           console.info("Time work in server => "+ lapTotalTime+" in seconds ,"+(lapTotalTime/time*100)+"%");
